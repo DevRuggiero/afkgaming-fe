4f0bfc3c600e3017881705e1c623a1f0380dfa3e
Ruggiero <ruggy9897@gmail.com>
2025-10-31 14:43:10 +0100

    ADD - AGGIUSTATO TAILWIND + RECENSIONI AGGIUNTE

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
 export class Homepage {
 
+  stars = Array(5).fill(0); // 5 stelle
+  reviews = [
+    {
+      nome: 'Luca R.',
+      img: 'https://i.pravatar.cc/60?img=1',
+      testo: 'Servizio impeccabile! Tutto è stato rapido e sicuro. Consigliatissimo.'
+    },
+    {
+      nome: 'Giulia M.',
+      img: 'https://i.pravatar.cc/60?img=2',
+      testo: 'Esperienza fantastica! Il sito è veloce e affidabile, tornerò sicuramente.'
+    },
+    {
+      nome: 'Marco D.',
+      img: 'https://i.pravatar.cc/60?img=3',
+      testo: 'Ottimo servizio clienti e consegna super rapida. Cinque stelle!'
+    }
+  ];
 }
+
+
